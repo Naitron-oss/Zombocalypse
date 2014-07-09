3b05a97702f0fcd9a03a48d1fde8c630a3df0324
slav <slavsan.mail@gmail.com>
2014-07-09 08:27:20 +0300

    zombies now move, hit, have images

--- js/zombie.js
+++ js/zombie.js
@@ -1,29 +1,71 @@
 (function( global ) {
   "use strict";
 
-  var WEAPON_PISTOL = 1;
-  var WEAPON_SHOTGUN = 2;
-  var WEAPON_MACHINE_GUN = 3;
+  var WEAPON_PISTOL = {name:'Pistol',className:'pistol',firepower:40};
+  var WEAPON_SHOTGUN = {name:'Shotgun',className:'shotgun',firepower:90};
+  var WEAPON_MACHINE_GUN = {name:'Machine gun',className:'shotgun',firepower:30};
 
-  var player;
+
 
   /**
    * Zombie
    * @constructor
    */
-  function Zombie() {
+  function Zombie( config ) {
+    this.doc = global.document;
+    this.holder = config.holder;
+    this.steps = 0;
+    this.strength = 10;
+    this.game = config.game;
+    this.player = config.player;
+    this.zIndex = config.zIndex;
     this.health = 100;
+    this.pic = this.getRandomRange(1,3);
     this.render();
+    this.bind();
     this.move();
+    var self = this;
+    this.direction = this.getRandomRange(1,2) === 1 ? 'left' : 'right';
+    //console.log('DIRECTION: ', this.direction);
+    this.interval = setInterval(function() {
+      self.move();
+    }, 1000);
   }
   Zombie.prototype = {
 
-    render: function() {
+    getRandomRange: function( min, max ) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
 
+    render: function() {
+      this.element = this.doc.createElement('div');
+      this.element.classList.add('zombie');
+      this.holder = this.doc.querySelector('.' + this.holder.className);
+      this.holder.appendChild(this.element);
+      this.element.style.left = this.getRandomRange(100, 600) + '%';
+      this.element.style.zIndex = this.zIndex;
+      this.element.style.width = '5px';
+      this.element.style.height = '10px';
+      this.element.style.background = 'url("images/zombie' + this.pic + '.gif") no-repeat center center';
+//      this.element.src = "images/zombie" + this.pic + ".gif";
     },
 
     move: function() {
+      //console.log('MOVE');
 
+      if (this.steps >= 100) {
+        this.player.damage(this.strength);
+        this.healthElement = 'H: ' + this.health;
+      } else {
+        this.steps += 1;
+        this.element.style.height = (parseInt(this.element.style.height, 10) + 2) + 'px';
+        this.element.style.width = (parseInt(this.element.style.width, 10) + 1) + 'px';
+        if (this.direction === 'left') {
+          this.element.style.left = (parseInt(this.element.style.left, 10) + 2) + 'px';
+        } else {
+          this.element.style.left = (parseInt(this.element.style.left, 10) - 1) + 'px';
+        }
+      }
     },
 
     updateProgress: function() {
@@ -32,14 +74,21 @@
 
     damage: function( damage ) {
       this.health -= damage;
+      //console.log('Damage: %i, health: %i', damage, this.health);
       this.updateProgress();
       if (this.health <= 0) {
         this.die();
       }
     },
 
-    die: function() {
+    bind: function() {
+      this.element.onclick = this.damage.bind(this, this.player.firepower);
+    },
 
+    die: function() {
+      clearInterval(this.interval);
+      this.game.kill(this);
+      this.holder.removeChild(this.element);
     }
   };
 
@@ -52,6 +101,8 @@
   function Player( config ) {
     this.health = 100;
     this.weapon = config.weapon;
+    this.game = config.game;
+    this.firepower = this.weapon.firepower;
     this.init();
   }
   Player.prototype = {
@@ -67,7 +118,7 @@
     },
 
     die: function() {
-
+      this.game.end();
     },
 
     init: function() {
@@ -88,32 +139,79 @@
     this.killed = 0;
     this.waves = 0;
     this.speed = 1000;
+    this.current = [];
+    this.count = 1;
+    this.zIndex = 100000000;
   }
   ZombieApocalypse.prototype = {
 
     render: function() {
       this.holder.classList.add('zombie-apocalypse-screen');
+      this.weaponElement = this.doc.createElement('div');
+      this.holder.appendChild(this.weaponElement);
+      this.weaponElement.classList.add('weapon', this.player.weapon.className);
+      this.healthElement = this.doc.createElement('div');
+      this.holder.appendChild(this.healthElement);
+      this.healthElement.textContent = 'H: ' + this.player.health;
+      this.healthElement.classList.add('health');
     },
 
     nextWave: function() {
+      var i, j = 0, self = this, interval;
       if (this.waves === 100) {
         this.quit();
       } else {
+        this.waves += 1;
+        this.count *= 2;
+        console.log('----------------');
+        console.log('Start wave: %i', this.waves);
+        console.log('Create %i zombies', this.count);
 
+
+        if (Math.floor(this.count / 10) > 0) {
+          console.log('HERE');
+          var temp = Math.floor(this.count / 10);
+          var count = 0;
+          interval = setInterval(function() {
+            for (i = 0; i < temp; i += 1, count += 1) {
+              if (self.count === count) {
+                clearInterval(interval);
+                break;
+              }
+              self.zIndex -= 1;
+              self.current.push(new Zombie({holder:self.holder, player:self.player, game:self, zIndex:self.zIndex}));
+            }
+          }, 2000);
+        } else {
+          for (i = 0; i < self.count; i += 1) {
+            self.zIndex -= 1;
+            self.current.push(new Zombie({holder:self.holder, player:self.player, game:self, zIndex:self.zIndex}));
+          }
+        }
+      }
+    },
+
+    kill: function( zombie ) {
+      var self = this;
+      this.killed += 1;
+      this.current.splice(zombie, 1);
+      console.log('Current zombies count: ', this.current.length);
+      if (this.current.length === 0) {
+        setTimeout(function() { self.nextWave(); }, 3000);
       }
     },
 
     bind: function() {
       this.doc.onkeyup = function( event ) {
         switch (event.keyCode) {
-          case 27: // Escape
+          case 27: // "Escape"
 
             break;
-          case 80: // P
+          case 80: // "P"
             this.pause();
             break;
           default:
-            console.log('Unhandled key code: ', event.keyCode);
+          //console.log('Unhandled key code: ', event.keyCode);
         }
       };
     },
@@ -126,13 +224,22 @@
       console.log('QUIT THE GAME');
     },
 
+    end: function() {
+      console.log('Game over');
+      for (var i = 0; i < this.current.length; i += 1) {
+        clearInterval(this.current[i].interval);
+      }
+      this.holder.classList.add('killed');
+    },
+
     init: function() {
       console.log('Start the game');
+      this.player = new Player({
+        weapon: WEAPON_PISTOL,
+        game: this
+      });
       this.bind();
       this.render();
-      player = new Player({
-        weapon: WEAPON_PISTOL
-      });
       this.nextWave();
     }
   };
